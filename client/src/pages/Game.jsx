001fe3b7aaf6a35b16936614094c2ceeafc98707
Kevin1678 <l21212351@tectijuana.edu.mx>
2026-03-10 20:37:39 -0700

    Update Game.jsx

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -37,9 +37,9 @@ export default function Game() {
     return () => destroy();
   }, [navigate]);
 
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <div id="phaser-root" style={{ width: "100%", height: "100%" }} />
-    </div>
+    return (
+  <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
+    <div id="phaser-root" style={{ width: "100%", height: "100%" }} />
+  </div>
   );
 }
