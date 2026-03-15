53b1e8ae8006e7d54b584348c4318a297e4e8b22
Kevin1678 <l21212351@tectijuana.edu.mx>
2026-03-14 19:37:23 -0700

    Delete server/prisma/migrations/20260314230228_add_parent_relation_type directory

--- server/prisma/migrations/20260314230228_add_parent_relation_type/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE `ParentStudent`
-    MODIFY `relationType` ENUM('FATHER', 'MOTHER', 'TUTOR', 'OTHER') NOT NULL DEFAULT 'OTHER';
-
--- CreateIndex
-CREATE INDEX `ParentStudent_studentId_relationType_idx`
-    ON `ParentStudent`(`studentId`, `relationType`);
