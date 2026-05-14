d843ba8b3e01cc72295818d745f9a6f2219df347
FerAB0403 <l21212322@tectijuana.edu.mx>
2026-05-13 22:07:51 -0700

    Add TeacherStudentResults route to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,10 +10,13 @@ import Admin from "./pages/Admin";
 import Game from "./pages/Game";
 import GamesMenu from "./pages/GamesMenu";
 import Protected from "./components/Protected";
+
 import Teacher from "./pages/Teacher";
 import TeacherDashboard from "./pages/TeacherDashboard";
 import TeacherStudents from "./pages/TeacherStudents";
 import TeacherParents from "./pages/TeacherParents";
+import TeacherStudentResults from "./pages/TeacherStudentResults";
+
 import Parent from "./pages/Parent";
 import ParentDashboard from "./pages/ParentDashboard";
 import ParentChildren from "./pages/ParentChildren";
@@ -42,6 +45,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           }
         >
           <Route index element={<TeacherDashboard />} />
+          <Route path="student-results" element={<TeacherStudentResults />} />
           <Route path="students" element={<TeacherStudents />} />
           <Route path="parents" element={<TeacherParents />} />
         </Route>
