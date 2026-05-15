7627f624a9f93adf51d267e55ef8fed20efe3e4a
Kevin1678 <l21212351@tectijuana.edu.mx>
2026-05-14 21:34:46 -0700

    Update topBar.js

--- client/src/phaser/lightsSequence/ui/topBar.js
+++ client/src/phaser/lightsSequence/ui/topBar.js
@@ -60,6 +60,12 @@ export function createTopUi(scene) {
 export function bindTopUiActions(scene) {
   scene.menuBtn.on("pointerdown", () => {
     if (scene.gameEnded && scene.endModal) return;
+
+    if (scene.requestAbandon) {
+      scene.requestAbandon(() => scene.scene.start("LightsMenuScene"));
+      return;
+    }
+
     scene.cleanupTransientState();
     scene.stopSpeechNow();
     scene.scene.start("LightsMenuScene");
@@ -67,6 +73,12 @@ export function bindTopUiActions(scene) {
 
   scene.exitBtn.on("pointerdown", () => {
     if (scene.gameEnded && scene.endModal) return;
+
+    if (scene.requestAbandon) {
+      scene.requestAbandon(() => scene._onExit?.());
+      return;
+    }
+
     scene.cleanupTransientState();
     scene.stopSpeechNow();
     scene._onExit?.();
